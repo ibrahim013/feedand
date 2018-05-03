b7cf1c1284c0f6f171038f223c5450b1794013e6
Charpell <charpellumeh@gmail.com>
2018-05-03 17:25:04 +0100

    Redux Setup

--- src/index.js
+++ src/index.js
@@ -2,12 +2,23 @@ import React from "react";
 import App from './App';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
 import './style/main.scss';
 
+import reducers from './reducers';
+
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
+
+
+
 ReactDOM.render(
-    <BrowserRouter>
+   <Provider store={store}> 
+       <BrowserRouter>
         <App />
     </BrowserRouter>
+    </Provider>
  ,
   document.getElementById("app"),
 );
