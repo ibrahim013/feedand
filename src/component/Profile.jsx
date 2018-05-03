3e16a52f199e3fa384bbc41e079d7c96cb3caab8
ibrahim013 <walaibrahim13@gmail.com>
2018-05-03 16:31:39 +0100

    route-created

---
+++ src/component/Profile.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export class Profile extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>this is an Profile page</h1>
+            </div>
+        )
+    }
+}
+export default Profile;
